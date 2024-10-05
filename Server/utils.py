552d0e74d40a1a0a38f52170a9a91c8c023aed1f
Sineshaw Legese <sinelegese306@gmail.com>
2024-10-05 17:03:47 +0300

    utils update

--- Server/utils.py
+++ Server/utils.py
@@ -6,12 +6,57 @@ import cv2
 from wavelet import toWavelet
 
 
+__class_name_to_number = {}
+__class_number_to_name = {}
+
+__model = None
+
 def classify_image(base_64_data, file_path=None):
-    print(base_64_data)
+    imgs = get_image_with_2_eyes(file_path, base_64_data)
+
+    result = []
+    for img in imgs:
+        scalled_raw_img = cv2.resize(img, (32, 32))
+        img_har = toWavelet(img, 'db1', 5)
+        scalled_img_har = cv2.resize(img_har, (32, 32))
+        combined_img = np.vstack((scalled_raw_img.reshape(32 * 32 * 3, 1), scalled_img_har.reshape(32 * 32, 1)))
+
+        len_image_array = 32*32*3 + 32*32
+
+        final = combined_img.reshape(1,len_image_array).astype(float)
+ 
+        result.append({
+            'class': class_number_to_name(__model.predict(final)[0]),
+            'class_probability': np.around(__model.predict_proba(final)*100,2).tolist()[0],
+            'class_dictionary': __class_name_to_number
+        })
+
+    return result
+
+def class_number_to_name(class_num):
+    return __class_number_to_name[class_num]
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global __class_name_to_number
+    global __class_number_to_name
+
+    with open("E:\\RoadMap\\ML Projects\\ML-Image-Classification\\Server\\Artifacts\\Class_dictionary.json", "r") as f:
+        __class_name_to_number = json.load(f)
+        __class_number_to_name = {v:k for k,v in __class_name_to_number.items()}
+
+    global __model
+    if __model is None:
+        with open("E:\\RoadMap\\ML Projects\\ML-Image-Classification\\Server\\Artifacts\\my_model.pkl", 'rb') as f:
+            __model = joblib.load(f)
+    print("loading saved artifacts...done")
+
+
+
 def get_base64_for_teddy():
-    with open('base64.txt') as file:
+    with open('E:\\RoadMap\\ML Projects\\ML-Image-Classification\\Server\\base64.txt') as file:
         return file.read()
-    
+
 def get_cv2_image_from_base64_string(b64str):
 
     encoded_data = b64str.split(',')[1]
@@ -41,14 +86,11 @@ def get_image_with_2_eyes(image_path, image_base64_data):
                 cropped_faces.append(roi_color)
     return cropped_faces
 
-def get_b64_test_image_for_virat():
-    with open("b64.txt") as f:
-        return f.read()
+
 
 if __name__ == '__main__':
     load_saved_artifacts()
 
-    print(classify_image(get_b64_test_image_for_virat(), None))
-
-if '__name__' == '__main__':
-    print(classify_image(get_base64_for_teddy(), None))
+    print(classify_image(get_base64_for_teddy(), 'E:\\RoadMap\\ML Projects\\ML-Image-Classification\\Server\\test image\\tedy.jpg'))
+    # print(classify_image(get_base64_for_teddy(), None))
+    # print(class_number_to_name(2))
